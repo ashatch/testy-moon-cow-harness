fed1fd179cad0949d6abc653123136eea00d00f0
Andrew Hatch <ahatch@amplience.com>
2020-06-26 11:20:17 +0100

    redirect stderr to null during test run

--- test-all-categories.sh
+++ test-all-categories.sh
@@ -6,9 +6,13 @@ run_category_test () {
    echo "Running category tests for $1"
    pushd $1 > /dev/null
    pwd
-   bash ./test.sh
+   bash ./test.sh 2> /dev/null
    popd > /dev/null
 }
+
+echo "Pulling latest submodules"
+
+git submodule update --init --recursive
 
 for categorydir in `find . -type d -name "moon-cow-*" -not -path '*/\.git/*'`
 do
